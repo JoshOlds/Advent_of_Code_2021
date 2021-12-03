06e583ec56beb3e4c9c12fb4f81aea9ef8bfcd8f
JoshOlds <jolds@bastiansolutions.com>
2021-12-02 18:22:48 -0700

    Adds time measurement to day 1. Adds WIP method to fetch advent puzzle input directly from web

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,27 +1,38 @@
-use adventlib;
+use std::time::{Instant};
 
+use adventlib;
 mod input;
 
 fn main() {
-    adventlib::print_puzzle_header(1);
+    adventlib::input_helpers::print_puzzle_header(1);
+    let iterations = 1000;
+    let start = Instant::now();
+    for _x in 0..iterations - 1{
+        run(false);
+    }
+    run(true);
+    let elapsed = (Instant::now() - start) / iterations;
+    println!("Elapsed time microseconds: {}", elapsed.as_micros());
+}
 
-    let input_vec: Vec<i32> = adventlib::split_string_to_vector(input::PUZZLE_INPUT, "\n");
-    let mut last_num = input_vec[0];
+fn run(do_print: bool) {
+    let input_vec: Vec<i32> = adventlib::input_helpers::split_string_to_vector(input::PUZZLE_INPUT, "\n");
     let mut count = 0;
 
-    // Part one calculation
-    for num in &input_vec
-    {
-        if *num > last_num{
-            count += 1 ;
-        }
-        last_num = *num;
-    }
-    println!("Part 1: {}", count);
+    // Part 1 calculation
+    input_vec
+        .windows(2)
+        .for_each(|w| {
+            if w[0] < w[1] { count += 1; }
+        } );
+    if do_print{ println!("Part 1: {}", count); }
 
     // Part 2 calculation
     count = 0;
-    let triplet_sums: Vec<i32> = input_vec.windows(3).map(|w| w[0] + w[1] + w[2]).collect();
+    let triplet_sums: Vec<i32> = input_vec
+        .windows(3)
+        .map(|w| w[0] + w[1] + w[2])
+        .collect();
     for (current, next) in triplet_sums.iter().zip(triplet_sums.iter().skip(1))
     {
         if *current < *next
@@ -30,6 +41,5 @@ fn main() {
         }
     }
 
-    println!("Part 2: {}", count);
-
+    if do_print { println!("Part 2: {}", count);}
 }
